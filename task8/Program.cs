08b2830a5a7ee9cd7d1ef3b8aebb583e7e087560
Kirill Grinkov <79636456163@yandex.ru>
2023-06-14 09:58:23 +0400

    task8done

---
+++ task8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string userImput = Console.ReadLine() ??"";
+int num = int.Parse(userImput);
+int div = 2;
+
+if(num > 1)
+{
+    while(div <= num)
+    {
+        Console.Write(div + " ");
+        div = div + 2;
+    }
+}
